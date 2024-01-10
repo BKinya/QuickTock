eea673bf734fcb657a5f3f66e6b5a502e086d64d
BKinya <gitongakinya1@gmail.com>
2024-01-10 10:48:04 +0300

    return us kotlin flow to return count down

--- app/src/main/java/com/beatrice/quicktock/data/repository/TimerRepository.kt
+++ app/src/main/java/com/beatrice/quicktock/data/repository/TimerRepository.kt
@@ -3,7 +3,7 @@ package com.beatrice.quicktock.data.repository
 import kotlinx.coroutines.flow.Flow
 
 interface TimerRepository {
-    suspend fun countDown(duration: Int): Int
+     fun doCountDown(duration: Int): Flow<Int>
 
     suspend fun setTimer(duration: Int): Flow<Boolean>
 
--- app/src/main/java/com/beatrice/quicktock/data/repository/TimerRepositoryImpl.kt
+++ app/src/main/java/com/beatrice/quicktock/data/repository/TimerRepositoryImpl.kt
@@ -3,13 +3,16 @@ package com.beatrice.quicktock.data.repository
 import com.beatrice.quicktock.data.datastore.TimerDataStore
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class TimerRepositoryImpl(
     private val dataStore: TimerDataStore,
 ) : TimerRepository {
-    override suspend fun countDown(duration: Int): Int {
-        delay(1000)
-        return duration - 1
+    override fun doCountDown(duration: Int): Flow<Int> = flow {
+        for(i in duration   downTo 0 step 1){
+            delay(1000)
+            emit(i)
+        }
     }
 
     override suspend fun setTimer(duration: Int): Flow<Boolean> = dataStore.setTimer(duration)
