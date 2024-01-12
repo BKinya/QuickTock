8a83e9cb55702d31d4c45f44c092351803074446
BKinya <gitongakinya1@gmail.com>
2024-01-12 13:17:15 +0300

    add a shell file

---
+++ healthCheck.sh
@@ -0,0 +1,7 @@
+ ./gradlew ktlintFormat  && ./gradlew ktlintCheck && ./gradlew test
+
+# When using it for the first time, use the following guarantee running permissions:
+# chmod +x healthCheck.sh
+#
+# After that, you just need to run:
+# ./healthCheck.sh
