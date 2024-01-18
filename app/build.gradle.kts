345314dc1c5d8a61f887bd740a7ad0a86f6f5496
BKinya <gitongakinya1@gmail.com>
2024-01-18 18:18:20 +0300

    clean up dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,46 +62,49 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
 
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    // Compose
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
-
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.test:runner:1.5.2")
-
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     testImplementation("androidx.compose.ui:ui-test-junit4")
 
+    // Tinder's state machine
     implementation("com.tinder.statemachine:statemachine:0.2.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
 
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+
+    // Koin
     val koinVersion = "3.5.3"
     implementation("io.insert-koin:koin-android:$koinVersion")
     implementation("io.insert-koin:koin-androidx-compose:$koinVersion")
+    testImplementation("io.insert-koin:koin-test:$koinVersion")
+    testImplementation("io.insert-koin:koin-test-junit5:$koinVersion")
 
+    // DataStore
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
+    // JUnit 5
     val jUnit5Version = "5.9.3"
     testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnit5Version")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnit5Version")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$jUnit5Version")
     testRuntimeOnly("org.junit.vintage:junit-vintage-engine:$jUnit5Version")
 
+    // Junit 4
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.robolectric:robolectric:4.11.1")
 
-    testImplementation("io.insert-koin:koin-test:$koinVersion")
-    testImplementation("io.insert-koin:koin-test-junit5:$koinVersion")
-
+    // turbine
     testImplementation("app.cash.turbine:turbine:1.0.0")
-    testImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    // Coroutines
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0-RC2")
 }
 tasks.withType<Test> {
